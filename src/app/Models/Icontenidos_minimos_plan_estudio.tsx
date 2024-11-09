f42c2ab8225738155fa8abd49b4795d11f5706bb
LuisO <lhorescovich@gmail.com>
2024-11-09 00:13:19 -0300

    Listo Modelo de contenidos minimos

--- src/app/Models/Icontenidos_minimos_plan_estudio.tsx
+++ src/app/Models/Icontenidos_minimos_plan_estudio.tsx
@@ -1,25 +1,47 @@
 import { Iasignatura } from "./Iasignatura";
+import { Iintensidad } from "./Iintensidad";
 import { Iplan_estudio } from "./Iplan_estudio";
 
 export interface Icontenidos_minimos_plan_estudio {
   contenidoMinimoPlanEstudioId: number;
-  nombre: string;
-  horasPractica?: number | 0;
-  horasTeoria?: number | 0;
-  exigencia: string;
-  //Agregar Plan de Estudio
-  planEstudioId: Iplan_estudio|null; //Pega la entidad entera
-  // planEstudio?: Iasignatura|null; //Tabla relacionada
-  asignaturaId: Iasignatura|null; //Pega la entidad entera
-  // asignatura?: Iasignatura|null; //Tabla relacionada
-  intensidadId: number|null; //Trae solamente el id
-  // intensidad?: Iintensidad|null; //Tabla relacionada
+  nombre?: string | null;
+  horasPractica?: number | null;
+  horasTeoria?: number | null;
+  exigencia?: string | null;
+  
+  asignaturaId?: number | null; 
+  asignatura?: Iasignatura | null; //Tabla relacionada
+  
+  planEstudioId?: number | null; //Pega la entidad entera
+  planEstudio?: Iplan_estudio | null; //Tabla relacionada
+  
+  intensidadId?: number | null;
+  intensidad?: Iintensidad | null; //Tabla relacionada
 }
 
-// "contenidoMinimoPlanEstudioId": "<integer>",
-// "nombre": "",
-// "horasPractica": "",
-// "horasTeoria": "",
-// "exigencia": "O",
-// "asignaturaId": "",
-// "intensidadId": ""
+// {
+//   "contenidoMinimoPlanEstudioId": 1,
+//   "nombre": "Historia de la Computación. Computación y sociedad",
+//   "horasPractica": null,
+//   "horasTeoria": null,
+//   "exigencia": null,
+
+//   "asignaturaId": 1,
+//   "asignatura": {
+//       "asignaturaId": 1,
+//       "codigo": "IF001",
+//       "nombre": "Elementos de Informática",
+//       "cargaHoraria": 105,
+//       "planEstudioId": {
+//           "planEstudioId": 1,
+//           "nombre": "Res CS 049-2017"
+//       }
+//   },
+//   "intensidadId": null,
+//   "intensidad": null,
+//   "planEstudioId": 1,
+//   "planEstudio": {
+//       "planEstudioId": 1,
+//       "nombre": "Res CS 049-2017"
+//   }
+// },
