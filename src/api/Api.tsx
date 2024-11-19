55a380b6a62fc5dea6e00aff789ba01384e65d3e
LuisO <lhorescovich@gmail.com>
2024-11-19 19:00:55 -0300

    baseURL

--- src/api/Api.tsx
+++ src/api/Api.tsx
@@ -2,8 +2,11 @@ import axios from 'axios';
 const apiUrl = import.meta.env.VITE_API_URL
 
 export const Api = axios.create({
-    baseURL: apiUrl
-    // baseURL: 'http://localhost:6543'
+    //*******Para Producción */
+    // baseURL: apiUrl
+        //*******Para Desarrollo */
+    baseURL: 'http://localhost:6543'
+
     // baseURL: '10.7.93.122:6543'
 })
 
